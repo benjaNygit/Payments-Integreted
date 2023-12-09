dc4f9c5913afe3f618b129c9861e13479e80e381
Benjamín Maldonado <benja.ignacio2001@gmail.com>
2023-12-09 18:32:58 -0300

    Navegación funcional

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import FormCreate from '../pages/Products/formCreate'
+
+function Navigate() {
+    return (
+        <>
+            <Link to='/products/create'>Crear Producto</Link>
+        </>
+    )
+}
+
 function Navbar() {
-    return <nav>
-        <a href="">Inicio</a>
-        <a href="">Productos</a>
-    </nav>
+    return (
+        <BrowserRouter>
+            <Navigate />
+            <Routes>
+                <Route path='/products/create' element={<FormCreate />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default Navbar
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './components/navbar'
-import FormCreate from './pages/Products/formCreate'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
       <Navbar />
-      <h1>Hello World!</h1>
-      <FormCreate />
     </>
   </React.StrictMode>,
 )
