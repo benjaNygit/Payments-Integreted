714b04dc5b4e8429e2d7f2627d7ffb2e65708136
Benjamín Maldonado <benja.ignacio2001@gmail.com>
2023-12-09 17:47:47 -0300

    Mejoras a la api

--- server/products/api.py
+++ server/products/api.py
@@ -2,7 +2,7 @@ from .models import Product
 from .serializers import ProductSerializer
 from rest_framework import viewsets, permissions
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductView(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ProductSerializer
--- server/products/urls.py
+++ server/products/urls.py
@@ -1,8 +1,13 @@
+from django.urls import path, include
 from rest_framework import routers
-from .api import ProductViewSet
+from rest_framework.documentation import include_docs_urls
+from .api import ProductView
 
 router = routers.DefaultRouter()
 
-router.register('api/products', ProductViewSet, 'products')
+router.register(r'products', ProductView, 'products')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+    path('docs/', include_docs_urls(title='Products API Documentation')),
+]
